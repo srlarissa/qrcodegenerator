c750946889905ae1727288052b83eeb624e9171e
Larissa Soares Reis <larissasoaresreis@gmail.com>
2025-07-24 14:11:21 -0300

    feat(adapter) Finalização do S3StorageAdapter

--- qrcode-generator/src/main/java/com/srlarissa/qrcode/generator/infrastructure/S3StorageAdapter.java
+++ qrcode-generator/src/main/java/com/srlarissa/qrcode/generator/infrastructure/S3StorageAdapter.java
@@ -1,4 +1,30 @@
 package com.srlarissa.qrcode.generator.infrastructure;
 
-public class S3StorageAdapter {
+import com.srlarissa.qrcode.generator.infrastructure.ports.StoragePort;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+public class S3StorageAdapter implements StoragePort {
+    private final S3Client s3Client;
+    private final String bucketName;
+    private final String region;
+
+    public S3StorageAdapter(@Value("${aws.s3.region}") String region,
+                            @Value("${aws.s3.bucket-name}") String bucketName){
+        this.bucketName = bucketName;
+        this.region = region;
+        this.s3Client = S3Client.builder()
+                .region(Region.of(this.region))
+                .build();
+    }
+
+    @Override
+    public String uploadFile(byte[] fileData, String fileName, String contentType) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(contentType)
+                .build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(fileData));
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
+    }
 }
