539f0e3f764aa130c3929d39dbf28205359c198c
Larissa Soares Reis <larissasoaresreis@gmail.com>
2025-07-25 14:57:48 -0300

    feat(controller): finaliza endpoint POST /qrcode com uso do QrCodeGeneratorService

--- qrcode-generator/src/main/java/com/srlarissa/qrcode/generator/controller/QrCodeController.java
+++ qrcode-generator/src/main/java/com/srlarissa/qrcode/generator/controller/QrCodeController.java
@@ -1,15 +1,29 @@
 package com.srlarissa.qrcode.generator.controller;
 import com.srlarissa.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.srlarissa.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.srlarissa.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/qrcode")
+
 public class QrCodeController {
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generateQrCode(@RequestBody QrCodeGenerateRequest request){
-        return null;
+        try{
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new QrCodeGenerateResponse("Error generating QR code: " + e.getMessage()));
+        }
     }
 }
