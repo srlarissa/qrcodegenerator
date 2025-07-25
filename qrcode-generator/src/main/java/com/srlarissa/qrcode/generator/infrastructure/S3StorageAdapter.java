21cde57c7c51e042a5e197237e0a8554e19ae34d
Larissa Soares Reis <larissasoaresreis@gmail.com>
2025-07-25 14:17:48 -0300

    feat(aws): finaliza implementação da classe S3StorageAdapter

--- qrcode-generator/src/main/java/com/srlarissa/qrcode/generator/infrastructure/S3StorageAdapter.java
+++ qrcode-generator/src/main/java/com/srlarissa/qrcode/generator/infrastructure/S3StorageAdapter.java
@@ -1,8 +1,14 @@
 package com.srlarissa.qrcode.generator.infrastructure;
 
 import com.srlarissa.qrcode.generator.infrastructure.ports.StoragePort;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+@Component
 public class S3StorageAdapter implements StoragePort {
     private final S3Client s3Client;
     private final String bucketName;
